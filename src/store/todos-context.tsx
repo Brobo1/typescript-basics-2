5991e46520d1ad503eb917428ed509d265f325e9
brobo1 <armanahz95@gmail.com>
2024-03-09 17:01:10 +0100

    Refactor todos-context.tsx code for improved readability and formatting consistency

--- src/store/todos-context.tsx
+++ src/store/todos-context.tsx
@@ -3,39 +3,38 @@ import {Todo} from "../models/todo";
 
 
 type TodosContextObj = {
-	items: Todo[],
-	addTodo: (text: string) => void,
-	removeTodo: (id: string) => void,
-}
+	items: Todo[];
+	addTodo: (text: string) => void;
+	removeTodo: (id: string) => void;
+};
 
 export const TodosContext = createContext<TodosContextObj>({
 	items:      [],
 	addTodo:    () => {},
 	removeTodo: () => {},
-}); 
+});
 
 export const TodosContextProvider: FC<{ children: ReactNode }> = (props) => {
 	const [todos, setTodos] = useState<Todo[]>([]);
 	
 	const addTodoHandler = (todoText: string) => {
-		setTodos(prevState => [new Todo(todoText), ...prevState]);
+		setTodos((prevState) => [new Todo(todoText), ...prevState]);
 	};
 	
 	const removeTodoHandler = (id: string) => {
-		setTodos(prevState => prevState.filter(value => (
-			value.id !== id
-		)))
+		setTodos((prevState) => prevState
+			.filter((value) => value.id !== id));
 	};
 	
 	const contextValue: TodosContextObj = {
 		items:      todos,
 		addTodo:    addTodoHandler,
 		removeTodo: removeTodoHandler,
-	}
+	};
 	
 	return (
 		<TodosContext.Provider value={contextValue}>
 			{props.children}
 		</TodosContext.Provider>
-	)
-}
+	);
+};
